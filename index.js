b64d28158b1c5698b79940675673e15ef692e59b
Petrovich1973 <designtorg@ya.ru>
2017-08-01 22:59:48 +0300

    modified

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
 });
 
 http.listen(port, function(){
-  console.log('listening on *:' + port);
+	console.log('listening on *:' + port);
 });
+//'192.168.1.2'
