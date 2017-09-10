53904bbb49185ed57f7353effb65ebf2040d48ee
Petrovich1973 <designtorg@ya.ru>
2017-09-10 17:19:33 +0300

    modern css

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
@@ -16,4 +17,3 @@ io.on('connection', function(socket){
 http.listen(port, function(){
 	console.log('listening on *:' + port);
 });
-//'192.168.1.2'
