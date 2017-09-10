9310877c0209a6467f370530f61cc1aa44915580
Petrovich1973 <designtorg@ya.ru>
2017-09-10 23:22:05 +0300

    добавил оформление

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # chat-example
+
+писать лог в файл
+nodemon --watch index.js > log-file.txt
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var moment = require('moment');
+
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
@@ -10,10 +12,12 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
 	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
+		var date = moment(new Date()).format('DD.MM.YY HH:mm:ss');
+		console.log(date + '\t', this.id + '\t', msg);
+		io.emit('chat message', msg, date, this.id);
 	});
 });
 
 http.listen(port, function(){
-	console.log('listening on *:' + port);
+	console.log('http://localhost:' + port);
 });
