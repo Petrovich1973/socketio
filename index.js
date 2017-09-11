2a6ffd8bb71187fbb61f4f11b5730d6627bc6ed2
Petrovich1973 <designtorg@ya.ru>
2017-09-12 00:55:29 +0300

    to fix

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require('express');  
+var app = express();  
+var server = require('http').createServer(app);  
+var io = require('socket.io')(server);
 
 var moment = require('moment');
 
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
-});
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 io.on('connection', function(socket){
 	socket.on('chat message', function(msg){
@@ -18,6 +18,6 @@ io.on('connection', function(socket){
 	});
 });
 
-http.listen(port, function(){
+server.listen(port, '0.0.0.0', function(){
 	console.log('http://localhost:' + port);
 });
